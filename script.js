ec861a5986daf4500377362581ddfc8442beb0ea
Sunny <o.puskas@hbk-bs.de>
2026-06-29 23:36:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function onpinguin() {
+  document.getElementById("pinguintext").style.display = "block";
+}
+function offpinguin() {
+  document.getElementById("pinguintext").style.display = "none";
+}
+
+function ontaube() {
+  document.getElementById("taubetext").style.display = "block";
+}
+function offtaube() {
+  document.getElementById("taubetext").style.display = "none";
+}
+
+function onpapagei() {
+  document.getElementById("papageitext").style.display = "block";
+}
+function offpapagei() {
+  document.getElementById("papageitext").style.display = "none";
+}
+
+function oneule() {
+  document.getElementById("euletext").style.display = "block";
+}
+function offeule() {
+  document.getElementById("euletext").style.display = "none";
+}
+
+function onaudio() {
+  document.getElementById("voice").style.display = "block";
+}
+function offaudio() {
+  document.getElementById("voice").style.display = "none";
+}
